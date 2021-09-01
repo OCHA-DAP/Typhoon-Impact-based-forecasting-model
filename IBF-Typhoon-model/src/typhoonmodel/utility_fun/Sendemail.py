15d3d41dc4ceb5dc02c30d272e879d4b77cdf0c5
turnerm <turnermon@gmail.com>
2021-09-01 21:34:12 +0200

    Revert "Add ehlo"
    This reverts commit 008d49eabc7c35f521688557b6db3586aaa94b90.

--- IBF-Typhoon-model/src/typhoonmodel/utility_fun/Sendemail.py
+++ IBF-Typhoon-model/src/typhoonmodel/utility_fun/Sendemail.py
@@ -37,7 +37,8 @@ def sendemail(smtp_server: str,
 
     # Open SSL connection and send the email
     context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, smtp_port, context=context) as server:
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls(context=context)
         server.login(email_username, email_password)
         server.sendmail(from_address,
                         to_address_list + cc_address_list,
